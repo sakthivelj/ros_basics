ea07fbcd9ec1d83b553eb38ad79a062bdcffd67f
sakthivelj <sakthivel1023@gmail.com>
2020-07-12 10:24:37 +0530

    ros_service done

--- ros_service/scripts/service_client.py
+++ ros_service/scripts/service_client.py
@@ -7,12 +7,20 @@ from ros_service.srv import AddTwoIntsRequest
 from ros_service.srv import AddTwoIntsResponse
 
 
-def add_two_ints_client(x,y):
-    
-def usage():
-    return "%s [x,y]"%sys.argv[0]
+def add_two_ints_client(x, y):
 
-if __name__ == '__main__':
+    rospy.wait_for_service('add_two_ints')
+    try:
+        add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
+        resp1 = add_two_ints(x, y)
+        return resp1.sum
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+def usage():
+    return "%s [x y]"%sys.argv[0]
+
+if __name__ == "__main__":
 
     if len(sys.argv) == 3:
         x = int(sys.argv[1])
@@ -20,4 +28,7 @@ if __name__ == '__main__':
     else:
         print usage()
         sys.exit(1)
-    print "Request %s+%s""%(x,y)
+
+    print "Requesting %s+%s"%(x,y)
+    s = add_two_ints_client(x,y)
+    print "%s + %s = %s"%(x, y, s)
--- ros_service/scripts/service_server.py
+++ ros_service/scripts/service_server.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python
+import rospy
+from ros_service.srv import AddTwoInts
+from ros_service.srv import AddTwoIntsRequest
+from ros_service.srv import AddTwoIntsResponse
+
+
+def handle_add_two_inits(req):
+
+    print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
+    return AddTwoIntsResponse(req.a + req.b)
+
+def add_two_ints_server():
+    rospy.init_node('add_two_ints_server')
+    s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_inits)
+    print "Ready to add two ints."
+    rospy.spin()
+
+if __name__=="__main__":
+
+    add_two_ints_server()
